73ab6df2ef8c35c7b226a1bdc04f46b584d54b71
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-05 09:26:28 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,30 @@
-# Write your code here!
+def game_hash 
+  game_hash ={
+    :home => {
+      :team_name => "Brooklyn Nets"
+      :colors [Black, White]
+      :players => {
+        :"Alan Anderson" => {
+          :number
+          :shoe
+          :points
+          :rebounds
+          :assists
+          :steals
+          :blocks
+          :slam_dunks
+        },
+        
+      }
+    }
+    :away => {
+      :team_name =>
+      :colors =>
+      :players =>
+    }
+    
+  }
+end
 
 
 
